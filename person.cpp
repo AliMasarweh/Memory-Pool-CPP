2202be78c03775d54ce6a5637eac7baebeb62cbd
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-17 11:53:42 -0800

    Finished the minimal methods without testing

--- person.cpp
+++ person.cpp
@@ -6,14 +6,14 @@
 #include <cstdlib>
 #include "person.h"
 
-template <size_t POOL_SIZE>
+template <size_t *POOL_SIZE>
 Person<POOL_SIZE>::Person(char fullname[32], unsigned int id, unsigned int age)
 :id(id), age(age)
 {
     strcpy(this->fullname, fullname);
 }
 
-template <size_t POOL_SIZE>
+template <size_t *POOL_SIZE>
 void *Person<POOL_SIZE>::operator new(size_t size) {
     if(s_firstFree == NULL)
         return NULL;
@@ -24,7 +24,7 @@ void *Person<POOL_SIZE>::operator new(size_t size) {
     return pointer;
 }
 
-template <size_t POOL_SIZE>
+template <size_t *POOL_SIZE>
 void Person<POOL_SIZE>::operator delete(void *ptr) {
     void** ptrToNext = reinterpret_cast<void**>(
             ((int*)ptr - sizeof(void*))
@@ -33,22 +33,28 @@ void Person<POOL_SIZE>::operator delete(void *ptr) {
     Person::s_firstFree = *ptrToNext;
 }
 
-template <size_t POOL_SIZE>
+template <size_t *POOL_SIZE>
 void *Person<POOL_SIZE>::operator new[](size_t count) {
     return NULL;
 }
 
-template <size_t POOL_SIZE>
+template <size_t *POOL_SIZE>
 void Person<POOL_SIZE>::operator delete[](void *ptr) {}
 
-template <size_t POOL_SIZE>
+template <size_t *POOL_SIZE>
 void *Person<POOL_SIZE>::init_pool() {
     size_t elements =
             Person::s_poolSize/(sizeof(Person)+sizeof(void*));
-    s_firstFree = s_pool;
+
+    s_firstFree = POOL_SIZE;
+    s_pool = POOL_SIZE;
+
+    void** current_pntr = static_cast<void**>(s_firstFree);
+
     for(size_t i = 0; i < elements; ++i)
     {
-
+        *current_pntr = current_pntr + sizeof(void*) + sizeof(Person);
+        current_pntr = static_cast<void**>(*current_pntr);
     }
 
     return s_pool;
