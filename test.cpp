fa657cd6c32059d0050328e1d8fb8323bc40715d
Ali Masarwe <alis.masarwe@gmail.com>
2020-02-17 10:28:59 -0800

    The basics build of the project

---
+++ test.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ali-masa on 2/17/20.
+//
+
+#include <gtest/gtest.h>
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(FooTest, DoesXyz) {
+
+}
